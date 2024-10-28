8f9073253ecd03e96718c668b2934a0dc646c52f
jmgdls <jazzymae.delossantos@gmail.com>
2024-10-28 10:22:00 -0700

    Fix user.py

--- src/api/user.py
+++ src/api/user.py
@@ -1,8 +1,2 @@
-// user.py
+# user.py
 
-from fastapi import APIRouter, Depends, Request
-from pydantic import BaseModel
-from src.api import auth
-
-import sqlalchemy
-from src import database as db
