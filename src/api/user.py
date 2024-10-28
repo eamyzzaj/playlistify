b25efbf3469c75d9e0515a47c35da48e280f5c65
jmgdls <jazzymae.delossantos@gmail.com>
2024-10-28 10:24:25 -0700

    Add imports and router

--- src/api/user.py
+++ src/api/user.py
@@ -1,2 +1,14 @@
 # user.py
 
+from fastapi import APIRouter, Depends, Request
+from pydantic import BaseModel
+from src.api import auth
+
+import sqlalchemy
+from src import database as db
+
+router = APIRouter(
+    prefix="/user",
+    tags=["user"],
+    dependencies=[Depends(auth.get_api_key)],
+)
