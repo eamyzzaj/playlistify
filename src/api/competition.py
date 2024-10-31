4313d58ab3a060d1ebee828de3745a31f1ffd840
Ivana <ivana_th@icloud.com>
2024-10-30 17:24:58 -0700

    added join competition endpoint

--- src/api/competition.py
+++ src/api/competition.py
@@ -5,6 +5,7 @@ from src.api import auth
 
 import sqlalchemy
 from src import database as db
+from src.api import user
 
 router = APIRouter(
     prefix="/competition",
@@ -27,3 +28,18 @@ def get_competitions():
                 }
             )
     return comp_list
+
+@router.post("/join")
+def join_competitions(username: str, compid: int ):
+    #update the competitions table (if competition is active) to update the number of active players
+    #add competition to user_competitions
+    with db.engine.begin() as connection:
+        get_user_id = connection.execute(sqlalchemy.text("SELECT user_id FROM users WHERE username = :passeduser"), {"passeduser": username}).scalar()
+        join_comp = connection.execute(sqlalchemy.text("""UPDATE competitions SET participants_count = participants_count + 1
+                                                        WHERE competition_id = :competitionid """), {"competitionid": compid})
+        add_to_user = connection.execute(sqlalchemy.text("""INSERT INTO usercompetitions(user_id, competition_id, enrollment_status, submission_status) 
+                                                         VALUES(:uid, :cid, TRUE, FALSE) """),
+                                                         {"uid": get_user_id, "cid":compid})
+        return {
+            "message": "OK"
+        }
