f86a28de610082caf565ee75dfd37f20a08b33cd
Ivana <ivana_th@icloud.com>
2024-11-04 18:09:52 -0800

    fixing join competitions

--- src/api/competition.py
+++ src/api/competition.py
@@ -35,6 +35,26 @@ def join_competitions(username: str, compid: int ):
     #add competition to user_competitions
     with db.engine.begin() as connection:
         get_user_id = connection.execute(sqlalchemy.text("SELECT user_id FROM users WHERE username = :passeduser"), {"passeduser": username}).scalar()
+
+        #first check if the user is in that competition:
+        is_in_comp = connection.execute(sqlalchemy.text("""
+            SELECT enrollment_status
+            FROM usercompetitions
+            WHERE usercompetitions.user_id = :passedid 
+            AND usercompetitions.competition_id = :competitionid"""), {"passedid": get_user_id, "competitionid":compid}).scalar()
+        is_active = connection.execute(sqlalchemy.text("""
+            SELECT status
+            FROM competitions
+            WHERE competition_id = :competitionid"""), {"competitionid": compid}).scalar()
+        if is_in_comp:
+            return {
+                "message": "already enrolled in competition"
+            }
+        
+        if is_active == "completed":
+            return {
+                "message": "competition has already concluded"
+            }
         join_comp = connection.execute(sqlalchemy.text("""UPDATE competitions SET participants_count = participants_count + 1
                                                         WHERE competition_id = :competitionid """), {"competitionid": compid})
         add_to_user = connection.execute(sqlalchemy.text("""INSERT INTO usercompetitions(user_id, competition_id, enrollment_status, submission_status) 
